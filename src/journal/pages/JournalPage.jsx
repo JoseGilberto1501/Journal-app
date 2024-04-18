7e0f5089bf9a868a0c2426a05530dea9c473a850
Jose Gilberto <joseg93151@outlook.com>
2024-04-17 21:36:26 -0600

    Upload new components, pages, router,theme, etc

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,13 @@
+
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+
+      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores a, quaerat reiciendis atque vero modi ducimus optio laudantium eius id quo voluptatum vitae animi earum? Est architecto cumque quibusdam nam.</Typography>
+      
+    </JournalLayout>
+  )
+}
