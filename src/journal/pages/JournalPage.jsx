4c98c5c89ce4c8c4c40cbed7c92cc573cb128106
Jose Gilberto <joseg93151@outlook.com>
2024-04-19 15:41:38 -0600

    finish template journal-app

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,32 @@
 
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores a, quaerat reiciendis atque vero modi ducimus optio laudantium eius id quo voluptatum vitae animi earum? Est architecto cumque quibusdam nam.</Typography>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores a, quaerat reiciendis atque vero modi ducimus optio laudantium eius id quo voluptatum vitae animi earum? Est architecto cumque quibusdam nam.</Typography>*/}
+
+      <NothingSelectedView />
+
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor: 'error.main',
+          ':hover' : { backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}>
+        <AddOutlined sx={{ fontSize:30 }}/>
+      </IconButton>
       
     </JournalLayout>
   )
 }
+//<NothingSelectedView />
+// <NoteView/>
